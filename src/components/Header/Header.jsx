3a9ba5ce845e7620b6354110c351ac1b395ab94e
fdg2112 <fdg2112@gmail.com>
2025-07-01 10:18:13 -0300

    main funcionando

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,9 @@ const Header = () => {
   return (
     <header className="header">
         <nav className="header-nav">
+          <Link to="/" className="header-logo">
+            <img src="/src/assets/logo.png" alt="Logo Mercado Liebre" className="header-logo-image" />
+          </Link>
           <Link to="/"><h1 className="header-title">Mercado Liebre</h1></Link>
           <ul className="header-menu">
             <li className="header-menu-item"><Link to="/login">Iniciar Sesión</Link></li>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,49 @@
+import { useState, useEffect } from "react";
 import "./Main.css";
 
 const Main = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        throw new Error("Error al obtener los productos");
+      }
+      const data = await response.json();
+      setProducts(data);
+    }
+    catch (error) {
+      setError("No se pudo cargar los productos. Detalle: " + error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <main className="main">
-        <p>Este es el Main</p>
+        <div className="main-header">
+          <h1>Productos</h1>
+        </div>
+        <div className="main-content">
+          {error && <p className="error">{error}</p>}
+          {products.length > 0 ? (
+            <ul className="product-list">
+              {products.map(product => (
+                <li className="product-item">
+                  <h2>{product.title}</h2>
+                  <p>Precio: ${product.price}</p>
+                  <img src={product.image} alt={product.title} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Cargando productos...</p>
+          )}
+        </div>
     </main>
   );
 }
