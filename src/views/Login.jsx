8e39f0f62054b4156798afa49a1cbb1194b8a3a0
fdg2112 <fdg2112@gmail.com>
2025-07-02 09:17:27 -0300

    login estilizado...

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,22 +3,28 @@ import "../styles/Login.css";
 
 const Login = () => {
   return (
-    <login className="login">
         <Layout>
-          <h2 className="login-title">Iniciar Sesión</h2>
-          <form className="login-form">
-              <div className="login-form-group">
-                  <label htmlFor="email">Correo Electrónico:</label>
-                  <input type="email" id="email" name="email" required />
-              </div>
-              <div className="login-form-group">
-                  <label htmlFor="password">Contraseña:</label>
-                  <input type="password" id="password" name="password" required />
-              </div>
-              <button type="submit" className="login-button">Iniciar Sesión</button>
-          </form>
+          <div className="login-container">
+            <div className="login-info">
+              <h2 className="login-title">Ingresá tu e-mail o teléfono para iniciar sesión</h2>
+            </div>
+            <div className="login-form-col">
+              <form className="login-form">
+                <div className="login-form-group">
+                  <label htmlFor="email">E-mail o teléfono</label>
+                  <input type="text" id="email" name="email" required />
+                </div>
+                <button type="submit" className="btn-primary">Continuar</button>
+                <button type="button" className="btn-secondary">Crear cuenta</button>
+              </form>
+              <div className="login-divider"><span>o</span></div>
+              <button type="button" className="btn-google">
+                <img src="/src/assets/google-icon.png" alt="" />  
+                Iniciar sesión con Google
+              </button>
+            </div>
+          </div>
         </Layout>
-    </login>
   );
 }
 
