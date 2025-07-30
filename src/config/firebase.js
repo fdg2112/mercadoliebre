b8a4a5a9c683627fd0debd20a4ec794d9220bc74
fdg2112 <fdg2112@gmail.com>
2025-07-30 08:12:16 -0300

    .

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,9 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
   apiKey: "AIzaSyA2SA1gBrGp9dBvzlPxAKtuwmOXJ1tRAiM",
   authDomain: "mercadoliebre-b1316.firebaseapp.com",
